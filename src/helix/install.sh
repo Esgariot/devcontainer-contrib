0c9f5af95ba9fa988e0c3ac66f75f3dbf737a571
Esgariot <esgariot@icloud.com>
2023-07-21 12:56:06 +0200

    fix: Do it manually.

--- src/helix/install.sh
+++ src/helix/install.sh
@@ -5,11 +5,28 @@ set -euo pipefail
 ensure_nanolayer nanolayer_location "v0.4.45"
 
 $nanolayer_location install apt-get git
+$nanolayer_location install apt-get curl
+$nanolayer_location install apt-get jq
 
-$nanolayer_location \
-    install \
-    devcontainer-feature \
-    "ghcr.io/devcontainers-contrib/features/gh-release:1.0.18" \
-    --option repo='helix-editor/helix' --option binaryNames='hx' --option libName="runtime" --option version="$VERSION" --option releaseTagRegex="\d{2}\.(0[1-9]|1[1-2])(\.\d+)?" #YY.0M(.MICRO) version 
+latest_ver="$(curl -sL https://api.github.com/repos/helix-editor/helix/releases/latest | jq -r ".tag_name")"
+ver="${latest_ver}"
+[[ "${VERSION}" =~ "latest" ]] || ver="${VERSION}"
 
-echo 'Done!'
+pkg="helix-${ver}-x86_64-linux"
+
+cd /tmp
+curl -fLO "https://github.com/helix-editor/helix/releases/download/${ver}/${pkg}.tar.xz"
+
+tar xJf "${pkg}.tar.xz"
+rm -f "${pkg}.tar.xz"
+
+# as in Arch's PKGBUILD for helix (https://gitlab.archlinux.org/archlinux/packaging/packages/helix)
+sed -i "s|hx|helix|g" "${pkg}"/contrib/completion/hx.*
+
+install -Dm 755 "${pkg}/hx" /usr/local/bin/helix
+install -Dm 644 "${pkg}/runtime" /usr/local/lib/helix/runtime
+
+install -Dm 644 "${pkg}/contrib/completion/hx.bash" "/usr/share/bash-completion/completions/helix"
+install -Dm 644 "${pkg}/contrib/completion/hx.fish" "/usr/share/fish/vendor_completions.d/helix.fish"
+install -Dm 644 "${pkg}/contrib/completion/hx.zsh" "/usr/share/zsh/site-functions/_helix"
+
