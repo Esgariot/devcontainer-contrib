0687e7f81ca1d92315bfaf59cd922a67ad16e372
Esgariot <esgariot@icloud.com>
2023-09-06 18:58:45 +0200

    misc: make sync

--- src/bat/install.sh
+++ src/bat/install.sh
@@ -1,15 +1,88 @@
 #!/usr/bin/env -S bash -i
 
-set -euo pipefail
-. ./library_scripts.sh
-ensure_nanolayer nanolayer_location "v0.4.45"
+set -Eeuo pipefail
+export DEBIAN_FRONTEND=noninteractive
 
-$nanolayer_location install apt-get git
+script_dir=$(cd "$(dirname "${0}")" &>/dev/null && pwd -P)
 
-$nanolayer_location \
-    install \
-    devcontainer-feature \
-    "ghcr.io/devcontainers-contrib/features/gh-release:1.0.18" \
-    --option repo='sharkdp/bat' --option binaryNames='bat' --option version="$VERSION"
+nl() { "${__install_nanolayer_cmd}" "$@"; }
 
-echo 'Done!'
+__step_install_nanolayer() {
+  . "${script_dir}/library_scripts.sh"
+  ensure_nanolayer __install_nanolayer_cmd "${nlver:-"v0.4.45"}"
+}
+
+__install_cleanup() {
+  rm -rf "/tmp/devcontainer_feature"
+}
+
+__step_install_dirs(){
+  trap '__install_cleanup' EXIT
+  srcdir="/tmp/devcontainer_feature/srcdir/${pkgname}"
+  pkgdir="/tmp/devcontainer_feature/pkgdir/${pkgname}"
+  mkdir -p "${srcdir}" && cd "${srcdir}"
+  mkdir -p "${pkgdir}"
+}
+
+__install_ensure_pkg() { dpkg-query -f='${Status:Want}' -W "${1}" || nl install apt-get "${1}"; }
+
+__step_install_depends(){
+  local missing_depends=()
+  join_by() { local IFS="$1"; shift; echo "$*"; }
+  for d in "${depends[@]}"; do
+    dpkg-query -f='${Status:Want}' -W "${d}" || missing_depends+=("${d}")
+  done
+  nl install apt-get "$(join_by , "${missing_depends[@]}")"
+}
+
+__step_install_pkgver() {
+  sed -i '0,/^\s*pkgver=/s/^\(s*\)pkgver=.*/\1pkgver="'"$(pkgver)"'"/' "${script_dir}/devcontainer-feature.sh"
+}
+__step_install_prepare() {
+  cd "${srcdir}"
+  prepare
+}
+
+__step_install_build() {
+  ldconfig
+  cd "${srcdir}"
+  build
+}
+
+__step_install_package() {
+  cd "${srcdir}"
+  package
+}
+
+__step_install_copy() {
+  (
+    shopt -s dotglob
+    shopt -s globstar
+    export GLOBIGNORE=".:.."
+    cd "${pkgdir}"
+    for i in **/*; do
+      [ ! -d  "${i}" ] && cp -P --parents "${i}" "/"
+    done
+  )
+}
+
+install_sh() {
+  while :; do
+    case "${1-}" in
+      --pkgver) __step_install_pkgver; exit;;
+      -?*) echo "Unknown option: $1"; exit 1 ;;
+      *) break ;;
+    esac
+    shift
+  done
+}
+
+. "${script_dir}/devcontainer-feature.sh"
+install_sh "$@"
+__step_install_nanolayer
+__step_install_dirs
+__step_install_depends
+__step_install_prepare
+__step_install_build
+__step_install_package
+__step_install_copy
