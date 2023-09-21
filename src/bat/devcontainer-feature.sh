a74c8b7110b9805c20e7e6fb1f2ae2729e2b2f82
Esgariot <esgariot@icloud.com>
2023-09-21 17:05:22 +0200

    fix: Parse pkgver.

--- src/bat/devcontainer-feature.sh
+++ src/bat/devcontainer-feature.sh
@@ -1,8 +1,13 @@
 pkgname="bat"
-pkgver="_"
+pkgver="0.23.0"
+url="https://github.com/sharkdp/${pkgname}"
 
 pkgver() {
-	:
+    case "${VERSION:-"latest"}" in
+        "latest") : "$(curl -Ls -o /dev/null -w %{url_effective} "${url}/releases/latest" | awk -F'/' '{print substr($NF,2)}')" ;;
+        *)        : "${VERSION}" ;;
+    esac
+    echo "$_"
 }
 
 prepare() {
@@ -14,5 +19,5 @@ build() {
 }
 
 package() {
-	nl install devcontainer-feature "ghcr.io/devcontainers-contrib/features/gh-release:1.0.18" --option repo='sharkdp/bat' --option binaryNames='bat' --option version="$VERSION"
+	nl install devcontainer-feature "ghcr.io/devcontainers-contrib/features/gh-release:1.0.18" --option repo='sharkdp/bat' --option binaryNames='bat' --option version="${pkgver}"
 }
