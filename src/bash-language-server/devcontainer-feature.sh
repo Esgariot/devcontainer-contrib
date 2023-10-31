baa8cf41d5a7f521cc93c4cc225cd4240c186e60
Esgariot <esgariot@icloud.com>
2023-10-31 12:48:07 +0100

    fix: Depend on new nvm.sh feature.
    Just try sourcing nvm.sh

--- src/bash-language-server/devcontainer-feature.sh
+++ src/bash-language-server/devcontainer-feature.sh
@@ -8,13 +8,6 @@ source=(
 depends=(shellcheck)
 
 _ensure_local_nvm() {
-  # ensure nvm
-  if command -v nvm >/dev/null 2>&1 || [ -d /usr/local/share/nvm ]; then 
-    :
-  else
-    echo "node.js is required"
-    exit 1
-  fi
   source /usr/local/share/nvm/nvm.sh || [[ $? != 1 ]]
 }
 
--- src/typescript-language-server/devcontainer-feature.sh
+++ src/typescript-language-server/devcontainer-feature.sh
@@ -7,13 +7,6 @@ source=(
 )
 
 _ensure_local_nvm() {
-  # ensure nvm
-  if command -v nvm >/dev/null 2>&1 || [ -d /usr/local/share/nvm ]; then 
-    :
-  else
-    echo "node.js is required"
-    exit 1
-  fi
   source /usr/local/share/nvm/nvm.sh || [[ $? != 1 ]]
 }
 
