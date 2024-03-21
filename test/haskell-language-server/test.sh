7a790a9234ebc1ce4368b00a0986d66de1176566
Esgariot <esgariot@icloud.com>
2024-03-21 23:54:54 +0100

    test: Add explicit local deps.

--- test/haskell-language-server/test.sh
+++ test/haskell-language-server/test.sh
@@ -2,6 +2,6 @@
 
 source dev-container-features-test-lib
 
-check "'haskell-language-server' executable is present in PATH and works" haskell-language-server-wrapper --version
+check "'haskell-language-server-wrapper' executable is present in PATH and works" haskell-language-server-wrapper --version
 
 reportResults
