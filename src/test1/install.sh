505c948a0a26be5913e82c9728e787db076fa4d4
Esgariot <esgariot@icloud.com>
2024-05-13 19:10:38 +0200

    refactor: Rewrite install.sh
    No longer use nanolayer.
    Don't bother with makedeb ppa, use .deb from github.

--- src/test1/install.sh
+++ src/test1/install.sh
@@ -2,132 +2,91 @@
 
 set -Eeuo pipefail
 set -x
-export DEBIAN_FRONTEND=noninteractive
 
+MAKEDEB_VERSION="16.1.0-beta1"
+UBUNTU_FLAVOR="$(grep -oP 'UBUNTU_CODENAME=\K\w+' /etc/os-release)"
+export DEBIAN_FRONTEND=noninteractive
 script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
 
-__step_source() {
-  . "${script_dir}/PKGBUILD"
-  metadir="/usr/local/share/devcontainer_feature/${pkgname}"
-  cachedir="/tmp/devcontainer_feature/cache/${pkgname}"
+# install dependencies
+apt-get update -qq
+apt-get install -qq sudo curl
+
+# install makedeb dependency
+command -v makedeb || {
+  makedebtmp="$(mktemp -d)"
+  curl -sfL -o "${makedebtmp}/makedeb.deb" "https://github.com/makedeb/makedeb/releases/download/v${MAKEDEB_VERSION}/makedeb-beta_${MAKEDEB_VERSION}_amd64_${UBUNTU_FLAVOR}.deb"
+  dpkg -i "${makedebtmp}/makedeb.deb"
+  
 }
 
-
-nl() { "${__install_nanolayer_cmd}" "$@"; }
-
-__step_nanolayer() {
-  . "${script_dir}/library_scripts.sh"
-  ensure_nanolayer __install_nanolayer_cmd "${nlver:-"v0.5.6"}"
+# create user that will install the features
+id devcontainer-feature &>/dev/null || {
+  useradd -c "devcontainer-feature" -G sudo -M -r -s /sbin/nologin devcontainer-feature
+  printf '%s ALL=(ALL:ALL) NOPASSWD: ALL\n' devcontainer-feature > /etc/sudoers.d/devcontainer-feature
 }
 
-__step_install_deps() {
-  nl install apt-get wget,gpg,sudo,curl #TODO: Instead of 'curl', add featdeps=() array for pre-makedeps
-  command -v makedeb >/dev/null || {
-    wget -qO - 'https://proget.makedeb.org/debian-feeds/makedeb.pub' | gpg --dearmor --batch --yes -o /usr/share/keyrings/makedeb-archive-keyring.gpg
-    echo 'deb [signed-by=/usr/share/keyrings/makedeb-archive-keyring.gpg arch=all] https://proget.makedeb.org/ makedeb main' | tee /etc/apt/sources.list.d/makedeb.list
-  }
-  nl install apt-get makedeb
-}
+# source PKGBUILD and set up variables and dirs for install locations
+. "${script_dir}/PKGBUILD"
+metadir="/usr/local/share/devcontainer_feature/${pkgname}"
+cachedir="/tmp/devcontainer_feature/cache/${pkgname}"
 
-__step_pkgver() {
-  local tempsuffix
-  local _pkgver
-  case "$(uname -s)" in
-    Darwin) tempsuffix='' ;;
-    Linux) tempsuffix="$(mktemp)";;
-  esac
-  _pkgver="$(pkgver)"
-  pkgver="${_pkgver}"
-  sed -i "${tempsuffix}" -e '1 s/^\s*pkgver=.*/pkgver="'"${_pkgver}"'"/; t' -e '1,// s//pkgver="'"${_pkgver}"'"/' "${1}" 
-}
+install -d -m 0775 -o root -g devcontainer-feature "${metadir}"
+install -d -m 0775 -o root -g devcontainer-feature "${cachedir}"
+install -D -t "${metadir}" "${script_dir}/PKGBUILD"
+cd "${metadir}"
 
-__step_add_user() {
-  id devcontainer-feature &>/dev/null || {
-    useradd -c "devcontainer-feature" -G sudo -M -r -s /sbin/nologin devcontainer-feature
-    printf '%s ALL=(ALL:ALL) NOPASSWD: ALL\n' devcontainer-feature > /etc/sudoers.d/devcontainer-feature
-  }
-}
+# fill out correct pkgver in PKGBUILD. Update locally too.
+pkgver="$(pkgver)"
+sed -i "$(mktemp)" -e '1 s/^\s*pkgver=.*/pkgver="'"${pkgver}"'"/; t' -e '1,// s//pkgver="'"${pkgver}"'"/' "${metadir}/PKGBUILD" 
 
-__step_install(){
-  install -d -m 0775 -o root -g devcontainer-feature "${metadir}"
-  install -d -m 0775 -o root -g devcontainer-feature "${cachedir}"
-  install -D -t "${metadir}" "${script_dir}/PKGBUILD"
-  cd "${metadir}"
-}
-
-__step_env() {
-  if [[ ${_buildenv:-} ]]; then
-    for e in "${_buildenv[@]}"; do
-      [[ "${!e:-}" ]] || continue
-      sed -i -e "1s;^;${e}=${!e}\n;" "${metadir}/PKGBUILD"
-    done
-  fi
-  printf '%s\n' "$(sha1sum "PKGBUILD" | head -c 40)" > "PKGBUILD.sha1sum"
-}
-
-__step_wrapper() {
-  local base="${pkgname}_${pkgver}-${pkgrel}_amd64"
-
-  case "${INSTALL:-"default"}" in
-    default) {
-			cat <<- EOF > "${metadir}/install.sh"
-				#!/usr/bin/env sh
-				
-				echo "${pkgname} has been installed."
-			EOF
-			chmod 755 "${metadir}/install.sh"
-			apt-get update
-			sudo -u devcontainer-feature env DEBIAN_FRONTEND=noninteractive makedeb "${metadir}/PKGBUILD" -sri --pass-env --no-confirm
-		};;
-		deferred) {
-			cat <<- EOF > "${metadir}/install.sh"
-				#!/usr/bin/env sh
-				set -eu
-				if [ "\$(id -u)" = "\$(id -u devcontainer-feature)" ]; then
-					:
-				else
-					echo "re-running as user=devcontainer-feature"
-					exec sudo -u devcontainer-feature "\$0" "\$@"
-				fi
-				stagedir="/tmp/devcontainer_feature/stage/${pkgname}"
-				sudo install -dm0775 -o root -g devcontainer-feature "\${stagedir}"
-				cd "\${stagedir}"
-				if [ "\$(cat ${cachedir}/${base}.PKGBUILD.sha1sum 2>/dev/null)" = "$(cat PKGBUILD.sha1sum)" ]; then
-					echo "Found matching deb package in cache. Installing..."
-					sudo dpkg -i "${cachedir}/${base}.deb"
-				else
-					echo "Installing..."
-					sudo apt-get update
-					install -m644 "${metadir}/PKGBUILD" PKGBUILD
-					env DEBIAN_FRONTEND=noninteractive makedeb PKGBUILD -sri --pass-env --no-confirm
-					sudo install -Dm644 "${metadir}/PKGBUILD.sha1sum" "${cachedir}/${base}.PKGBUILD.sha1sum"
-					sudo install -Dm644 "${base}.deb" "${cachedir}/${base}.deb"
-				fi
-			EOF
-			chmod 755 "${metadir}/install.sh"
-			install -Dm0440 <(printf '%s ALL=(devcontainer-feature) NOPASSWD: %s\n' ALL "${metadir}/install.sh") "/etc/sudoers.d/devcontainer-feature_${pkgname}"
-		};;
-	esac
-}
-
-install_sh() {
-  while :; do
-    case "${1-}" in
-      --pkgver) __step_pkgver "${script_dir}/PKGBUILD"; exit;;
-      -?*) echo "Unknown option: $1"; exit 1 ;;
-      *) break ;;
-    esac
-    shift
+if [[ ${_buildenv:-} ]]; then
+  for e in "${_buildenv[@]}"; do
+    [[ "${!e:-}" ]] || continue
+    sed -i -e "1s;^;${e}=${!e}\n;" "${metadir}/PKGBUILD"
   done
+fi
+printf '%s\n' "$(sha1sum "PKGBUILD" | head -c 40)" > "${metadir}/PKGBUILD.sha1sum"
 
-  __step_nanolayer
-  __step_install_deps
-  __step_add_user
-  __step_source
-  __step_install
-  __step_pkgver "${metadir}/PKGBUILD"
-  __step_env
-  __step_wrapper
-}
+# create wrapper script
+BASE_NAME="${pkgname}_${pkgver}-${pkgrel}_amd64"
+case "${INSTALL:-"default"}" in
+  default) {
+		cat <<- EOF > "${metadir}/install.sh"
+			#!/usr/bin/env sh
+			
+			echo "${pkgname} has been installed."
+		EOF
+		chmod 755 "${metadir}/install.sh"
+		sudo -u devcontainer-feature env DEBIAN_FRONTEND=noninteractive makedeb "${metadir}/PKGBUILD" -sri --pass-env --no-confirm
+	};;
+	deferred) {
+		cat <<- EOF > "${metadir}/install.sh"
+			#!/usr/bin/env sh
+			set -eu
+			if [ "\$(id -u)" = "\$(id -u devcontainer-feature)" ]; then
+				:
+			else
+				echo "re-running as user=devcontainer-feature"
+				exec sudo -u devcontainer-feature "\$0" "\$@"
+			fi
+			stagedir="/tmp/devcontainer_feature/stage/${pkgname}"
+			sudo install -dm0775 -o root -g devcontainer-feature "\${stagedir}"
+			cd "\${stagedir}"
+			if [ "\$(cat ${cachedir}/${BASE_NAME}.PKGBUILD.sha1sum 2>/dev/null)" = "$(cat PKGBUILD.sha1sum)" ]; then
+				echo "Found matching deb package in cache. Installing..."
+				sudo dpkg -i "${cachedir}/${BASE_NAME}.deb"
+			else
+				echo "Installing..."
+				sudo apt-get update
+				install -m644 "${metadir}/PKGBUILD" PKGBUILD
+				env DEBIAN_FRONTEND=noninteractive makedeb PKGBUILD -sri --pass-env --no-confirm
+				sudo install -Dm644 "${metadir}/PKGBUILD.sha1sum" "${cachedir}/${BASE_NAME}.PKGBUILD.sha1sum"
+				sudo install -Dm644 "${BASE_NAME}.deb" "${cachedir}/${BASE_NAME}.deb"
+			fi
+		EOF
+		chmod 755 "${metadir}/install.sh"
+		install -Dm0440 <(printf '%s ALL=(devcontainer-feature) NOPASSWD: %s\n' ALL "${metadir}/install.sh") "/etc/sudoers.d/devcontainer-feature_${pkgname}"
+	};;
+esac
 
-install_sh "$@"
