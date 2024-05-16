0d10a6c6942b5a5d9c096d33a2eefe75c18b310a
Robert Marciniak <esgariot@icloud.com>
2024-05-16 17:15:19 +0200

    fix: `_ghc_ver` was empty in `package()`. (#11)
    * fix: _ghc_ver was empty in package().
    
    * fix: Remove merge leftover.
    
    * misc: Update version.
    
    * test: Add hls typecheck test.

--- test/haskell-language-server/default.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-source dev-container-features-test-lib
-
-check "'haskell-language-server-wrapper' executable is present in PATH and works" haskell-language-server-wrapper --version
-
-reportResults
--- test/haskell-language-server/test.sh
+++ test/haskell-language-server/test.sh
@@ -4,4 +4,14 @@ source dev-container-features-test-lib
 
 check "'haskell-language-server-wrapper' executable is present in PATH and works" haskell-language-server-wrapper --version
 
+mkdir hello_hs
+cat <<- EOF > "hello_hs/hello.hs"
+module Main where
+
+main :: IO ()
+main = putStrLn "hello"
+EOF
+
+check "'haskell-language-server-wrapper' can typecheck simple program" haskell-language-server-wrapper typecheck "hello_hs/hello.hs"
+
 reportResults
